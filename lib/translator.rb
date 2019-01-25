1cc9b5b78786c3e2103f0511ff6c40bb56259046
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-25 08:13:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   lib = {get_emoticon => {}, get_meaning =>{}}
   emoticons.each do |emotion, emoticon|
-    lib[get_meaning[emoticon[1]] = emotion
+    lib[get_meaning][emoticon[1]] = emotion
     get_emoticon[emoticon[0]] = emoticon[1]
   end
   lib
