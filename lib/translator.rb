e2ef6394647336baa766dc0366022e98687453b3
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-25 08:25:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if result
     result
   else
-    puts "Sorry, that emoticon was not found"
+    "Sorry, that emoticon was not found"
   end
 end
 
@@ -26,6 +26,6 @@ def get_english_meaning(file_path, emoticon)
   if result
     result
   else
-    puts "Sorry, that emoticon was not found"
+    "Sorry, that emoticon was not found"
   end
 end
