ac67e8bef378690046868951350ae34c2e26a3fb
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-25 07:58:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,9 @@ require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
+  get_emoticon = {}
+  get_meaning = {}
+  emoticons.each do |
 end
 
 def get_japanese_emoticon
